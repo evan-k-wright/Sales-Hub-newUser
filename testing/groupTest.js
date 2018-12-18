d46616bcb72da1461b30b7630f16e12a734e9da2
Evan Wright <rideburtonsnow3@gmail.com>
2018-12-18 15:19:43 -0700

    adding code

--- testing/groupTest.js
+++ testing/groupTest.js
@@ -42,5 +42,26 @@ module.exports = {
                 .waitForElementVisible('@AAA', 5000)
     },
 
-    
+    'Edit User': (browser) => {
+        let editUser = browser.page.editUser();
+        editUser.navigate()
+                //edit user
+                    .click('@AAA')
+                    .waitForElementVisible('@firstName', 5000)
+                    .waitForElementVisible('@lastName', 5000)
+                    .waitForElementVisible('@changePassword', 5000)
+                    .click('@changePassword')
+                    .waitForElementVisible('@userPassword', 5000)
+                    .clearValue('@firstName')
+                    .clearValue('@lastName')
+                    .setValue('@firstName', data.editFirstName)
+                    .setValue('@lastName', data.editLastName)
+                    .setValue('@userPassword', data.editUserPassword)
+                    .api.useXpath()
+                    .moveToElement('//a[.="Manage assigned groups"]', 0, 0)
+                    editUser.waitForElementVisible('@admin',5000)
+                    .click('@admin')
+                    .click('@save')
+                    .waitForElementPresent('@AAA333', 5000)
+            }
 }
